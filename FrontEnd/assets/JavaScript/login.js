84732821f5a76cc4543737878a1dec2ff6bb9a53
LB-Squishy <laurentleboulbar@gmail.com>
2023-05-15 17:05:49 +0200

    2.2 Authentification - Gestion connexion & token

--- FrontEnd/assets/JavaScript/login.js
+++ FrontEnd/assets/JavaScript/login.js
@@ -1,9 +1,38 @@
-// Récupération des éléments du formulaire de connection----------------------------------------------------
+// Connexion et gestion du bouton "Se connecter"--------------------------------------------------------------
 
 const loginForm = document.querySelector(".loginForm")
 loginForm.addEventListener ("submit", function(event) {
     event.preventDefault()
-    const emailUser = document.querySelector("#email").value
-    const passwordUser = document.querySelector("#password").value
-    console.log(emailUser, passwordUser)
+    //récupération des éléments du formulaire
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+    //envoi de la requête en backend
+    fetch('http://localhost:5678/api/users/login', {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify ({
+            email, 
+            password
+        })
+    })
+    //vérif de la réponse et retour JSON
+    .then (function(response) {
+        if(response.ok) {
+            return response.json()
+        }
+    })
+    //stockage du token et redirection
+    .then (function(data) {
+        const token = data.token
+        localStorage.setItem("token", token)
+        window.location.href = "index.html"
+    })
+    //gestion des erreurs d identification
+    .catch (function(error) {
+        console.error ("Erreur dans l'identifiant ou le mot de passe")
+    })
 })
+
+// Suite => Intégration bandeau de modification admin et gestion de son apparition au log
+//       => Voir si necessite de l envoi du Token sur le Js gallery ou si le localstorage suffit
+//       => quand sur page index faire un test en saisissant token dans la console pour voir ce qu il en est
