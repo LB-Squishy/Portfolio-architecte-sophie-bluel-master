3606c9f1d2569245edfade4824b534d7ead07b36
LB-Squishy <laurentleboulbar@gmail.com>
2023-05-19 18:13:09 +0200

    1.2 "..." - refacto filtrage et gestion dynamique

--- FrontEnd/assets/JavaScript/index.js
+++ FrontEnd/assets/JavaScript/index.js
@@ -1,9 +1,4 @@
-// Mettre une classe correspondant à l id de la categorie en valeur num sur chaque boutons
-// Et créer une fonction qui filtre en fonction de l id recuperé sur la classe du bouton
-// pour regrouper la gestion des boutons générer en une seule fonction
-// Finir la mise page une fois Log (vérifier les marge et créer le bouton edition qui genere la modale)
-
-// Récupération des travaux et des catégories depuis l'API----------------------------------------------------------------------
+// Récupération des travaux et des catégories depuis l'API-----------------------------------------------------
 const responseWorks = await fetch('http://localhost:5678/api/works/')
 const galleryData = await responseWorks.json()
 console.log(galleryData)
@@ -11,25 +6,32 @@ const responseCategories = await fetch('http://localhost:5678/api/categories/')
 const categoriesData = await responseCategories.json()
 console.log(categoriesData)
 
-// Appel du Token si présent
+// Appel du Token si présent----------------------------------------------------------------------------------
 const token = localStorage.getItem("token")
 console.log(token)
 
 // Fonction de création des boutons de filtrage---------------------------------------------------------------
 const filters = document.querySelector(".filters")
-function generateFilters(arrayData) {
+function generateFiltersBtn(arrayData) {
+    // Bouton Tous
     const buttonAllProject = document.createElement("button")
     buttonAllProject.innerText = "Tous"
     buttonAllProject.classList.add("filtersButton", "filtersAllProject")
     filters.appendChild(buttonAllProject)
+    // Bouton par catégorie
     arrayData.forEach(categorie => {
         const buttonFilter = document.createElement("button")
         buttonFilter.innerText = categorie.name
-        buttonFilter.classList.add("filtersButton", "filters"+categorie.name.replace(/\s/g, "").replace(/&/g, ""))
+        buttonFilter.classList.add(
+            "filtersButton", 
+            "filtersProject",
+            "filters"+categorie.name.replace(/\s/g, "").replace(/&/g, ""),
+            categorie.id
+            )
         filters.appendChild(buttonFilter)
     })
 }
-generateFilters(categoriesData)
+generateFiltersBtn(categoriesData)
 
 // Fonction de création de galerie----------------------------------------------------------------------------
 const gallery = document.querySelector(".gallery")
@@ -52,36 +54,28 @@ function generateGallery(arrayData) {
 // Initialisation de la page ---------------------------------------------------------------------------------
 generateGallery(galleryData)
 
-// Gestion des boutons de filtrage ---------------------------------------------------------------------------
-    // Bouton Tous
+// Fonction de filtrage par catégorie--------- ---------------------------------------------------------------
 const allProjectButton = document.querySelector(".filtersAllProject")
-allProjectButton.addEventListener ("click", function(){
-    document.querySelector(".gallery").innerHTML = ""
-    generateGallery(galleryData)
-})
-    // Bouton Objets
-const objectButton = document.querySelector(".filtersObjets")
-objectButton.addEventListener ("click", function(){
-    document.querySelector(".gallery").innerHTML = ""
-    const objectArray = galleryData.filter(project => project.category.id === 1)
-    generateGallery(objectArray)
-})
-    // Bouton Appartements
-const apartmentButton = document.querySelector(".filtersAppartements")
-apartmentButton.addEventListener ("click", function(){
-    document.querySelector(".gallery").innerHTML = ""
-    const apartmentArray = galleryData.filter(project => project.category.id === 2)
-    generateGallery(apartmentArray)
-})
-    // Bouton Hôtel & Restaurant
-const hotelsRestaurantButton = document.querySelector(".filtersHotelsrestaurants")
-hotelsRestaurantButton.addEventListener ("click", function(){
-    document.querySelector(".gallery").innerHTML = ""
-    const hotelsRestaurantArray = galleryData.filter(project => project.category.id === 3)
-    generateGallery(hotelsRestaurantArray)
-})
+const filtersButton = document.querySelectorAll(".filtersProject")
+function generateFilters() {
+    // Bouton Tous
+    allProjectButton.addEventListener ("click", function(){
+        document.querySelector(".gallery").innerHTML = ""
+        generateGallery(galleryData)
+    })
+    // Bouton par catégorie
+    filtersButton.forEach(button => {
+        button.addEventListener ("click", function(){
+            const categorieId = parseInt (this.classList[3])
+            document.querySelector(".gallery").innerHTML = ""
+            const filterArray = galleryData.filter(project => project.category.id === categorieId)
+            generateGallery(filterArray)
+        })
+    })
+}  
+generateFilters()
 
-// Gestion mode admin ou mode public------------------------------------------------------------
+// Gestion mode admin ou mode public--------------------------------------------------------------------------
     // récupération des éléments
 const editTool = document.querySelector(".editTool")
 const body = document.querySelector("body")
